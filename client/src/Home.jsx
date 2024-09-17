02e605f2fba64497cc955fc94fc0567b377c9110
shyamkumar1999 <kapavarapushyamkumar0@gmail.com>
2024-09-17 16:44:58 +0530

    commit message

---
+++ client/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <div>
+     <h2>HOME COMPONENT</h2>
+    </div>
+  )
+}
+
+export default Home
+
+
+
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require("mongoose")
+const cors = require('cors')
+const EmployeeModel = require('./models/Employee')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/employee") 
+
+
+app.post('/login',(req,res) => {
+    const{email,password} = req.body
+    EmployeeModel.findOne({email: email})
+    .then(user =>{
+        if(user){
+            if(user.password === password){ 
+                res.json('Success')
+            }else{
+                res.json("the password is incorrect")
+            }
+        }else{
+            res.json("No Record Existed")
+        }
+    })
+
+})
+
+app.post("/register", (req,res) => { 
+    EmployeeModel.create(req.body)
+    .then(employees => res.json(employees))
+    .catch(err => res.json(err)) 
+})
+
+
+
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log(`server is running ${4000}`)
+     
+})
